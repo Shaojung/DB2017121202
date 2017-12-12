fe29f12259fc3afe0e668f3b78548d82c55c0c60
Shaojung <shaojung@gmail.com>
2017-12-12 20:03:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2017121202/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.student.db2017121202;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v4.graphics.BitmapCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    class MyTask extends AsyncTask<String, Integer, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+
+            try {
+                // create the HttpURLConnection
+                url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // 使用甚麼方法做連線
+                connection.setRequestMethod("GET");
+
+                // 是否添加參數(ex : json...等)
+                //connection.setDoOutput(true);
+
+                // 設定TimeOut時間
+                connection.setReadTimeout(15 * 1000);
+                connection.connect();
+                InputStream is = connection.getInputStream();
+                // 伺服器回來的參數
+
+
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
